fd8df0a8387df2c0b03ec7d951ab6cf9a708bcc8
IanRoss409 <68993175+IanRoss409@users.noreply.github.com>
2024-12-10 09:23:28 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  return {
+    name: 'Member',
+    skills: [
+      'JavaScript',
+      'TypeScript',
+      'React',
+      'React Native',
+      'Node.js',
+      'GraphQL',
+            'AWS'
+          ]
+        };
+      }
